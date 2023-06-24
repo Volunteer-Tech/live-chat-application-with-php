5c515dd3a1063a5b25868427bfca9fa6a2931db1
Anna Arihu <136196182+annarihu@users.noreply.github.com>
2023-06-23 23:43:17 -0700

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    require "config/config.php";
+    $id = $_SESSION['id'];
+    $username = $_SESSION['username'];
+    $sql = "UPDATE users SET logged=0 WHERE id=".$id."";
+    mysqli_query($link, $sql);
+    $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just disconnected!')";
+    mysqli_query($link, $sql);
+    session_reset();
+    session_destroy();
+    header("location: home.php");
+    exit;
+?>
